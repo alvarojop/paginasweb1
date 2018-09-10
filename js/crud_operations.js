c35cb28784621228273ac80ec7331629633d8929
Alvaro J. Ortiz P <alvar@AJOP-HP.AJOP-HP>
2018-09-09 20:14:57 -0600

    llamado de paginas por botones

--- js/crud_operations.js
+++ js/crud_operations.js
@@ -16,5 +16,29 @@ $(document).ready(function(){
             $(this).html(data);
         });
     });
-    
+
+    $('#btncreate').click(function () {
+        $('#divPartialView').load('html/create.html', function(data) {
+            $(this).html(data);
+        });
+    });
+
+    $('#btndetail').click(function () {
+        $('#divPartialView').load('html/read.html', function(data) {
+            $(this).html(data);
+        });
+    });
+
+    $('#btnedit').click(function () {
+        $('#divPartialView').load('html/update.html', function(data) {
+            $(this).html(data);
+        });
+    });
+
+    $('#btndelete').click(function () {
+        $('#divPartialView').load('html/delete.html', function(data) {
+            $(this).html(data);
+        });
+    });
+
 });
