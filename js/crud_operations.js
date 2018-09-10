328e3dc8fd60b4a819ef2effa3d7a0a66e54a888
Alvaro J. Ortiz P <alvar@AJOP-HP.AJOP-HP>
2018-09-09 19:05:35 -0600

    creacion de la lista de facturas y el llamado a dicha pagina

--- js/crud_operations.js
+++ js/crud_operations.js
@@ -1,24 +1,20 @@
 $(document).ready(function(){
 
-    $("#btntop").click(function() {
+    $('#btntop').click(function() {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
     });
 
-    $("#btninicio").click(function() {
-        $("#dinicio").attr('class', 'visible text-center');
-        $("#dpicosita").attr('class', 'invisible text-center');
-        $("#dmichelada").attr('class', 'invisible text-center');
-        $("#djagerbomb").attr('class', 'invisible text-center');
-        $("#dmojito").attr('class', 'invisible text-center');
-        $("#dmargarita").attr('class', 'invisible text-center');
+    $('#btninicio').click(function () {
+        $('#divPartialView').load('html/inicio.html', function(data) {
+            $(this).html(data);
+        });
+    });
 
-        $("#dinicio").attr('style', 'display:block');
-        $("#dpicosita").attr('style', 'display:none');
-        $("#dmichelada").attr('style', 'display:none');
-        $("#djagerbomb").attr('style', 'display:none');
-        $("#dmojito").attr('style', 'display:none');
-        $("#dmargarita").attr('style', 'display:none');
+    $('#btnfacturas').click(function () {
+        $('#divPartialView').load('html/list.html', function(data) {
+            $(this).html(data);
+        });
     });
     
 });
